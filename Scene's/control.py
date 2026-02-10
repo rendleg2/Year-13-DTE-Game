1c0732dfa1b6a30c75d252660bce06902af8bfa1
QNH10132 <romanmetcalfe@gmail.com>
2026-02-10 14:21:13 +1300

    started database script

---
+++ Scene's/control.py
@@ -0,0 +1,39 @@
+
+from py4godot.methods import private
+from py4godot.signals import signal, SignalArg
+from py4godot.classes import gdclass
+from py4godot.classes.core import Vector3
+from py4godot.classes.Control import Control
+
+	#howdy
+
+@gdclass
+class control(Control):
+	database : SQLite 
+	 def _ready(self) -> None:  #bleh
+		database = SQLite.new()
+		database.path="res://data.db"
+		database.open_db()    #cch
+	# define properties like this
+	
+	test_int: int = 5
+	test_float: float = 5.2
+	test_bool: bool = True
+	test_vector: Vector3 = Vector3.new3(1,2,3)
+
+	# define signals like this
+	test_signal = signal([SignalArg("test_arg", int)])
+
+
+	def _ready(self) -> None:
+		pass
+		# put initialization code here
+
+	def _process(self, delta:float) -> None:
+		pass
+		# put dynamic code here
+
+	# Hide the method in the godot editor
+	@private
+	def test_method(self):
+		pass
