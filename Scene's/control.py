9321ec1271ef1a6c7ab1b070e6f16a86af2070ca
QNH10132 <romanmetcalfe@gmail.com>
2026-02-12 14:18:47 +1300

    created command1 for database
    "practise databse" following turtorial

--- Scene's/control.py
+++ Scene's/control.py
@@ -4,36 +4,22 @@ from py4godot.signals import signal, SignalArg
 from py4godot.classes import gdclass
 from py4godot.classes.core import Vector3
 from py4godot.classes.Control import Control
-
+import sqlite3
 	#howdy
 
 @gdclass
 class control(Control):
-	database : SQLite 
-	 def _ready(self) -> None:  #bleh
-		database = SQLite.new()
-		database.path="res://data.db"
-		database.open_db()    #cch
-	# define properties like this
-	
-	test_int: int = 5
-	test_float: float = 5.2
-	test_bool: bool = True
-	test_vector: Vector3 = Vector3.new3(1,2,3)
 
-	# define signals like this
-	test_signal = signal([SignalArg("test_arg", int)])
-
-
-	def _ready(self) -> None:
-		pass
-		# put initialization code here
-
+	def _ready(self) -> None:  #bleh
+		connection = sqlite3.connect('store_transaction.db')
+		cursor = connection.cursor()
+		#different
+		command1 = """create table if not exists
+		stores(store_id integer primary key, location text)"""
+		cursor.execute(command1)
+		# purchasing table
+		#command2 = """create table if not exists
+		#purchases(purchase_id integer primary key, """
 	def _process(self, delta:float) -> None:
 		pass
 		# put dynamic code here
-
-	# Hide the method in the godot editor
-	@private
-	def test_method(self):
-		pass
