bf68e2cbcbc91fbac2b4e5edc3a5aa062a486b29
rendleg2 <20136@rosmini.school.nz>
2026-02-13 13:28:26 +1300

    added some comments to code

--- Scripts/enemy_template.gd
+++ Scripts/enemy_template.gd
@@ -13,7 +13,7 @@ var aggro = false
 @onready var navigation_agent_2d = $NavigationAgent2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready() -> void: #spawn in the enemys weapon
 	navigation_agent_2d.target_position = Target_Position # Delete once testing complete
 	if weapon:
 		weaponInstance = weapon.instantiate()
@@ -26,14 +26,12 @@ func _process(delta: float) -> void:
 		_on_follow_follow()
 	Pathfind(delta)# move into aggro if statement once testing complete
 
-func _on_follow_follow():
-	# will set pathfind desination to player
+func _on_follow_follow(): # will set pathfind desination to player
 	if Target_Position != Target.position:
 		Target_Position = Target.position
 		navigation_agent_2d.target_position = Target_Position
 		
-func Pathfind(delta):
-	#moves the enemy closer to the next pathfind point
+func Pathfind(delta): #moves the enemy closer to the next pathfind point
 	Current_Position = self.global_position
 	Next_Step = navigation_agent_2d.get_next_path_position()
 	Velocity = Current_Position.direction_to(Next_Step) * Movement_Speed
@@ -45,7 +43,6 @@ func Pathfind(delta):
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
-
 
 func _on_area_2d_body_entered(body): # if target enters detection range, attack
 	if body == Target:
