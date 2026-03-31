1fb30fce466171e33df9a96eb294587261bea676
QNH10132 <romanmetcalfe@gmail.com>
2026-04-01 10:50:37 +1300

    touch up on stats.db

--- scripts/Database/stats_database.py
+++ scripts/Database/stats_database.py
@@ -11,7 +11,7 @@ class stats_database(Node):
 	def _ready(self) -> None:
 		self.conn = sqlite3.connect('stats.db')
 		self.cursor = self.conn.cursor()
-		c = conn.cursor()
+		c = self.conn.cursor()
 		print(c)
 		self.cursor.execute("""CREATE TABLE IF NOT EXISTS stats (
 			id INTEGER PRIMARY KEY,
@@ -19,5 +19,11 @@ class stats_database(Node):
 		)""")
 		self.cursor.execute("SELECT health FROM stats WHERE id = 1")
 		if self.cursor.fetchone() is None:
-			self.cursor.execute("INSERT INTO stats (id, health)")
+			self.cursor.execute("INSERT INTO stats (id, health) VALUES (1,100)")
 			self.conn.commit()
+		def update_health(self, new_health_value: int) -> None:
+				"""Call this function whenever health changes"""
+				self.cursor.execute("UPDATE stats SET health = ? WHERE id = 1", (new_health_value,))
+				self.conn.commit()
+				print(f"Database Updated: Health is now {new_health_value}")
+			
