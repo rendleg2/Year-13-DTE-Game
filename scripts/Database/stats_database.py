975af237b7c5edaac7fdeb7e6f46570e4c30807c
QNH10132 <romanmetcalfe@gmail.com>
2026-04-22 10:01:09 +1200

    finished database healthbar

--- scripts/Database/stats_database.py
+++ scripts/Database/stats_database.py
@@ -21,12 +21,19 @@ class stats_database(Node):
 		if self.cursor.fetchone() is None:
 			self.cursor.execute("INSERT INTO stats (id, health) VALUES (1,100)")
 			self.conn.commit()
+		
 		def update_health(self, new_health_value: int) -> None:
 				"""Call this function whenever health changes"""
 				self.cursor.execute("UPDATE stats SET health = ? WHERE id = 1", (new_health_value,))
 				self.conn.commit()
 				print(f"Database Updated: Health is now {new_health_value}")
+			
 		def get_health(self) ->int:
 			"""Call this to load the health from the database"""
 			self.cursor.execute("Select health from stats where id = 1")
-			
+			result = self.conn.fetchone()
+			return result[0] if result else 0 
+		
+		def _exit_tree(self) -> None:
+			self.conn.close()
+		
