7a5f263f80015826498d081e5c9e11f04dd27c0e
rendleg2 <20136@rosmini.school.nz>
2026-05-11 10:59:30 +1200

    fixed offshoot bug, stoping procedual gen form working

--- scripts/game/procedual_generation.gd
+++ scripts/game/procedual_generation.gd
@@ -53,10 +53,11 @@ func _ready() -> void:
 	end_coords = Vector2i(x, y)
 	
 	rooms_grid[start_coords.x][start_coords.y] = 0
-	rooms_grid[end_coords.x][end_coords.y] = -1
+	
 	#create initial path
 	path_bitwise(start_coords, end_coords, max_attempts)
 	#place the rooms onto playable area
+	rooms_grid[end_coords.x][end_coords.y] = 0
 	await off_shoot_rooms()
 	place_rooms_bitwise()
 	#place_rooms()
@@ -90,14 +91,38 @@ func off_shoot_rooms():
 					skip = randi_range(5, 10)
 					offshoot_count += 1
 					offshoot_points.append([Vector2i(x, y), dir.pick_random()])
+					
 				else:
 					skip-=1
 					
 	for i in range(0,len(offshoot_points)):
-		print(offshoot_points)
-		print(offshoot_points[i][0], " <> ", rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y])
-		rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y] += bitwise[offshoot_points[i][1]]
-		path_bitwise(Vector2i(offshoot_points[i][0].x, offshoot_points[i][0].y), null, 5)
+		var check = 0
+		print(offshoot_points[i][1], " off, dir")
+		print(offshoot_points[i][0], " <> ", rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y], " off")
+		print(rooms_grid[offshoot_points[i][0].x+offshoot_points[i][1].x][offshoot_points[i][0].y+offshoot_points[i][1].y])
+		
+		if offshoot_points[i][0].x-1 >= 0 and rooms_grid[offshoot_points[i][0].x-1][offshoot_points[i][0].y] == -1 and offshoot_points[i][1] != Vector2i.LEFT:
+			rooms_grid[offshoot_points[i][0].x-1][offshoot_points[i][0].y] = 0
+		elif offshoot_points[i][0].x-1 >= 0 and rooms_grid[offshoot_points[i][0].x-1][offshoot_points[i][0].y] == -1 and offshoot_points[i][1] == Vector2i.LEFT:
+			check +=1
+		if offshoot_points[i][0].x+1 <= rooms_grid_size.x and rooms_grid[offshoot_points[i][0].x+1][offshoot_points[i][0].y] == -1 and offshoot_points[i][1] != Vector2i.RIGHT:
+			rooms_grid[offshoot_points[i][0].x+1][offshoot_points[i][0].y] = 0
+		elif offshoot_points[i][0].x+1 <= rooms_grid_size.x and rooms_grid[offshoot_points[i][0].x+1][offshoot_points[i][0].y] == -1 and offshoot_points[i][1] == Vector2i.RIGHT:
+			check +=1
+		if offshoot_points[i][0].y-1 >= 0 and rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y-1] == -1  and offshoot_points[i][1] != Vector2i.UP:
+			rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y-1] = 0
+		elif offshoot_points[i][0].y-1 >= 0 and rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y-1] == -1  and offshoot_points[i][1] == Vector2i.UP:
+			check +=1
+		if offshoot_points[i][0].y+1 <= rooms_grid_size.y and rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y+1] == -1 and offshoot_points[i][1] != Vector2i.DOWN:
+			rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y+1] = 0
+		elif  offshoot_points[i][0].y+1 <= rooms_grid_size.y and rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y+1] == -1 and offshoot_points[i][1] == Vector2i.DOWN:
+			check +=1
+		if check > 0:
+			rooms_grid[offshoot_points[i][0].x][offshoot_points[i][0].y] += bitwise[offshoot_points[i][1]]
+			path_bitwise(Vector2i(offshoot_points[i][0].x, offshoot_points[i][0].y), null, 5)
+			print("making path ", check)
+		else:
+			print("not making path", check)
 		
 
 func place_rooms_bitwise():
@@ -109,8 +134,8 @@ func place_rooms_bitwise():
 				for i in range(0, len(room_scenes)):
 					
 					var name:String = room_scenes[i].resource_path.get_file().get_basename()
-					print(name, " looking for> ", rooms_grid[x][y])
-					print(i, " <> ", name.contains(str(rooms_grid[x][y])))
+					#print(name, " looking for> ", rooms_grid[x][y])
+					#print(i, " <> ", name.contains(str(rooms_grid[x][y])))
 					if name.to_int() != rooms_grid[x][y]:
 						continue
 					else:
@@ -120,37 +145,6 @@ func place_rooms_bitwise():
 				stamp_room(possible_rooms.pick_random(), Vector2i(x*room_size, y*room_size), 0)
 			elif rooms_grid[x][y] != 0 and rooms_grid[x][y] != -1:
 				print("FAIL TO FIND ROOM ", rooms_grid[x][y])
-
-func place_rooms():
-	var selected_room
-	for x in range(0, rooms_grid_size.x+1):
-		for y in range(0, rooms_grid_size.y+1):
-			if len(rooms_grid[x][y]) > 0 and rooms_grid[x][y] != [0]:
-				#print(" new room ")
-				#$Floor.set_cell(Vector2i(x, y), 0, Vector2i(1, 1), 0)
-				for i in range(0, len(room_scenes)):
-					selected_room = "error"
-					var Match =true
-					var name:String = room_scenes[i].resource_path.get_file().get_basename()
-					#print(name, " looking for> ", rooms_grid[x][y])
-					for z in rooms_grid[x][y]:
-						#print(z, " <> ", name.contains(str(z)))
-						if name.contains(str(z)) != true:
-							Match = false
-							break
-						else:
-							name = name.erase(name.find(str(z)), str(z).length())
-						
-					if Match:
-						print(name)
-						for check in [Vector2i.DOWN, Vector2i.UP, Vector2i.LEFT, Vector2i.RIGHT]:
-							if name.contains(str(check)) == true:
-								Match = false
-					if Match:
-						selected_room = i
-						break
-				stamp_room(selected_room, Vector2i(x*room_size, y*room_size), 0)
-
 
 
 func stamp_room(id, coord, type):
@@ -173,84 +167,6 @@ func stamp_room(id, coord, type):
 	$Floor.set_pattern(coord, floor.get_pattern(floor.get_used_cells()))
 	$Walls.set_pattern(coord, wall.get_pattern(wall.get_used_cells()))
 	room.queue_free()
-
-func path(start_coords_2, end_coords_2, max_attempts_2):
-	# 1 up , 2 down , 3 left , 4 right
-	var dir = []
-	var current_coords = start_coords_2
-	var last_coords = Vector2i.ZERO
-	var move = Vector2i(0,0)
-	var last_move = Vector2i.ZERO
-	var x
-	var y
-	var attempts = -1
-	while current_coords != end_coords_2 and attempts != max_attempts_2:	
-		attempts += 1
-		dir = []
-		var new = current_coords
-		
-		#possible directons
-		if new.x-1 >= 0 and rooms_grid[new.x-1][new.y] == []:
-			dir.append(Vector2i.LEFT)
-		if new.x+1 <= rooms_grid_size.x and rooms_grid[new.x+1][new.y] == []:
-			dir.append(Vector2i.RIGHT)
-		if new.y-1 >= 0 and rooms_grid[new.x][new.y-1] == []:
-			dir.append(Vector2i.UP)
-		if new.y+1 <= rooms_grid_size.y and rooms_grid[new.x][new.y+1] == []:
-			dir.append(Vector2i.DOWN)
-
-		#Backtracking
-		if end_coords_2 == null and len(dir) == 0:
-			print("broke: ", attempts)
-			break
-		elif len(dir) == 0:
-			if path_taken.size() == 0:
-				print("Pathfinder stuck at start, no route possible!")
-				break
-			rooms_grid[new.x][new.y] = [0]
-			current_coords -= path_taken[-1][1]
-			path_taken.pop_back()
-			new = current_coords
-		
-		else:
-			#print(move)
-			if attempts >0:
-				if move == Vector2i.UP or move == Vector2i.DOWN:
-					last_move=-move
-				else:
-					last_move=-move
-				last_coords = current_coords
-			#random or best move?
-			if (randi_range(0, 100) >= max_windingness or len(dir) == 2) and end_coords_2 != null:
-				move = dir.pick_random()
-				for direction in dir:
-					if (current_coords + direction).distance_to(end_coords_2) < current_coords.distance_to(end_coords_2):
-						move = direction
-			else:
-				move = dir.pick_random()
-			
-			new += move
-			
-			#saving the move
-			if attempts > 0:
-				path_taken.append([last_move,move])
-				#print(current_coords.x)
-				rooms_grid[last_coords.x][last_coords.y] = [last_move, move]
-			else:
-				last_move=-move
-
-			current_coords = new
-		#print(new, " ", move)
-	
-	if current_coords != end_coords_2 and end_coords_2 != null:
-		#print(rooms_grid)
-		print("fail")
-	elif current_coords == end_coords_2:
-		#print(rooms_grid)
-		print("sucsess")
-		#print(path_taken)
-	else:
-		print("extra path hit end of road")
 
 func path_bitwise(start_coords_2, end_coords_2, max_attempts_2):
 	
@@ -280,6 +196,8 @@ func path_bitwise(start_coords_2, end_coords_2, max_attempts_2):
 		#Backtracking
 		if end_coords_2 == null and len(dir) == 0:
 			print("broke: ", attempts)
+			if attempts !=0:
+				rooms_grid[current_coords.x][current_coords.y] = bitwise[last_move]
 			break
 		elif len(dir) == 0:
 			if path_taken.size() == 0:
@@ -325,6 +243,7 @@ func path_bitwise(start_coords_2, end_coords_2, max_attempts_2):
 		print("sucsess")
 		#print(path_taken)
 	else:
+		rooms_grid[current_coords.x][current_coords.y] = bitwise[last_move]
 		print("extra path hit end of road")
 
 #command .exe / work = true == yes = no bc I said yes and it works so yes - work # real!! #its working - print work
